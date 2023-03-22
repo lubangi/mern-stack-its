9491f7cf672fdd064376e64db49ce4b20d762f35
lubangi <lionnel@LIONNELs-MacBook-Pro.local>
2023-03-22 08:13:26 +0200

    Final code its

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ActivityRouter = require("./routes/activity.route");
+const cors = require("cors");
+
+
+const app = express();
+app.use(cors());
+
+/* Loading the environment variables from the .env file. */
+require("dotenv").config();
+
+//mongo
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/todoapiDB";
+
+/* Telling the application to use the express.json() middleware. This middleware will parse the body of
+any request that has a Content-Type of application/json. */
+app.use(express.json());
+
+/* Telling the application to use the ActivityRouter for any requests that start with "/api". */
+app.use("/api", ActivityRouter);
+
+/* This is a route handler. It is listening for a GET request to the root route of the application.
+When it receives a request, it will send back a response with the string "Hello World!". */
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+
+/* Connecting to the database and then starting the server. */
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(PORT, console.log("Server stated on port 5000"));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
